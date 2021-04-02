4fcec68dcbcc2d32fad13f09341c31b1d47f7f5d
dev-csh <dev-csh@naver.com>
2021-04-02 17:55:38 +0900

    add controller DAO VO img

--- src/view/Grade_Controller.java
+++ src/view/Grade_Controller.java
@@ -9,6 +9,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -16,6 +18,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,10 +27,31 @@ import java.util.logging.Logger;
 
 public class Grade_Controller implements Initializable {
 
+	@FXML
+	private TableView<Product> grade_tableview;
+	
+	@FXML
+	private TableColumn<Product, Integer> table_view_number;
+	
+	@FXML
+	private TableColumn<Product, String> table_view_contents;
+	
+	@FXML
+	private TextField member_name_text;
+	
+	@FXML
+	private TextField member_grade_text;
+	
+	
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-
+		grade_init();
+	}
+	
+	private void grade_init() {
+		member_name_text.setText("User1");
+		member_grade_text.setText("5.0");
 	}
 
 }
