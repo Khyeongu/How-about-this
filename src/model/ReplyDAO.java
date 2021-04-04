9de58d879605393653fbb650c9f0d5d22b0303e9
JunSuHeo <wmfkf95@gmail.com>
2021-04-04 19:22:13 +0900

    Rent Detail 수정

--- src/model/ReplyDAO.java
+++ src/model/ReplyDAO.java
@@ -2,9 +2,58 @@ package model;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import database.DBConnection;
+import oracle.jdbc.OracleTypes;
 
 public class ReplyDAO {
 	// db관련 객체
 		private Connection conn;
-		private CallableStatement callableStatement;
+		private CallableStatement cs;
+		
+		private ReplyVO replyVO = new ReplyVO();
+		
+		public ArrayList<ReplyVO> getAllReplyList(int boardId){
+			ArrayList<ReplyVO> replyList = new ArrayList<>();
+			
+			String runSP = "{ call select_reply_board (?, ?) }";
+			
+			try {
+				conn = DBConnection.getConnection();
+				cs = conn.prepareCall(runSP);
+				
+				cs.setInt(1, boardId); // 카테고리id 받아와서 할당하는 부분
+				cs.registerOutParameter(2, OracleTypes.CURSOR);
+				System.out.println();
+
+				try {
+					cs.execute();
+					ResultSet resultSet = (ResultSet) cs.getObject(2);
+
+					while (resultSet.next()) {
+						
+						replyVO.setContent(resultSet.getString(2));
+						replyVO.setTime(resultSet.getDate(3));
+						System.out.println("title: " + replyVO.getContent());
+						System.out.println("price: " + replyVO.getTime().toString());
+						System.out.println();
+						replyList.add(new ReplyVO(
+								resultSet.getInt(1), resultSet.getString(2),
+								resultSet.getDate(3), resultSet.getInt(4)));
+					}
+					
+					return replyList;
+
+				} catch (SQLException e) {
+					System.out.println("프로시저에서 에러 발생!");
+					System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return replyList;
+		}
 }	
